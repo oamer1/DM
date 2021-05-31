cf03598ea9c7a3a564912980a0dcdd20a79cd5f3
macjag10 <67389207+macjag10@users.noreply.github.com>
2021-05-31 07:25:49 -0700

    Update qmail.py

--- dm/qmail.py
+++ dm/qmail.py
@@ -1,3 +1,4 @@
+
 import getpass
 import smtplib
 from email.mime.application import MIMEApplication
@@ -5,9 +6,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pathlib import Path
 from typing import Dict, List
-
 import jinja2
-
 
 def send_email(
     subject: str,
@@ -33,47 +32,42 @@ def send_email(
         "JIRA_ticket": "JIRA_ticket.html",
     }
     user = getpass.getuser()
+
     if command_template == "request_branch":
         subject = f"New Branch Requested by {user}"
     else:
         subject = f"Wtf {command_template} command"
-
     commands = list(Email_templates.keys())
 
     if command_template not in commands:
         raise ValueError(f"Unknown Command : {command_template} not in {commands}")
-
     Email_file = Email_templates[command_template]
     # Load EMAIL_Submit.html template
-    # File_path = str(Path(os.environ["RFA_MODELERS_DIR"]) / "dm")
-    env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(
-            searchpath="/prj/analog/blaster_eval/sandiego/chips/reference/reference_v100/work/mgajjar/REFERENCE/verif_modules/rfa_modelers/python3/dm/Email_template/"
-        )
-    )
-    # env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath = File_path))
+  
+    
+    RFA_DIR = Path(os.environ["RFA_MODELERS_DIR"])
+    path_dir = str(RFA_DIR / "python3" /"dm" / "Email_template" )
+    
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath = path_dir))
     mail_template = env.get_template(Email_file)
+
     # Place contents in content template placeholder
     html_body = mail_template.render(content=content)
-
     msg = MIMEMultipart()
     msg["Subject"] = subject
     msg["From"] = sender
     msg["To"] = ", ".join(recipients)
     msg.attach(MIMEText(html_body, "html"))
-
     # Attach log file if a path is passed
     if attachment:
         file_path = attachment
         file_name = file_path.name
-
         with open(file_path, "rb") as f:
             log_file_attachement = MIMEApplication(f.read())
             log_file_attachement.add_header(
                 "Content-Disposition", "attachment", filename=file_name
             )
             msg.attach(log_file_attachement)
-
     s = smtplib.SMTP(smtp_host)
     s.send_message(msg)
     s.quit()
