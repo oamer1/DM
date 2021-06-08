2d6c414ff10b6ca7ce228417d782df07366ec160
Omar Amer <oamer@windowslive.com>
2021-06-08 15:21:35 +0200

    Refactor options choice in set_ws command into choose_option function.

--- dm/sitar.py
+++ dm/sitar.py
@@ -1117,11 +1117,9 @@ def set_ws(args: argparse.Namespace, config: ConfigParser) -> int:
             filtered_areas = all_areas_names
 
         print("Please choose one of these workspaces:")
-        for i, area in enumerate(filtered_areas, 1):
-            print(i, area)
+        choice = choose_option(filtered_areas)
 
-        choice = ask_option_number(len(filtered_areas), f"(1-{i})")
-        ws_section = f"area:{filtered_areas[choice].lower()}"
+        ws_section = f"area:{choice.lower()}"
 
     ws = config[ws_section]
     ws_name = ws["name"]
