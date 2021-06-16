3ee8727e019783631b1505e386bb2cf389b59a0b
macjag10 <67389207+macjag10@users.noreply.github.com>
2021-06-16 13:56:15 -0700

    Latest

--- dm/Wtf_dm.py
+++ dm/Wtf_dm.py
@@ -5,6 +5,8 @@ Supports running SITaR commands via a DM shell.
 import argparse
 import getpass
 import os
+import re
+import tempfile
 from pathlib import Path
 from typing import List
 
@@ -73,10 +75,12 @@ class Wtf_dm(dm.Sitar_dm):
                 f"status = {self.sitr_mods[mod]['status']}"
             )
             if only_update:
-                if self.sitr_mods[mod]["status"] != "Update":
+                if self.sitr_mods[mod]["status"] != "Update mode":
                     LOGGER.warn(f"The module {mod} is not in update mode")
                     continue
             self.modules.append(mod)
+        if not self.modules:
+            self.modules = self.sitr_mods
 
     def populate(self, force: bool = False) -> int:
         """Populate a SITaR workspace"""
@@ -91,11 +95,7 @@ class Wtf_dm(dm.Sitar_dm):
 
     def pop_modules(self, force: bool = False) -> int:
         """Populate all modules in update mode in the SITaR workspace"""
-        if self.modules:
-            mod_list = self.modules
-        else:
-            mod_list = self.sitr_mods
-        self.dssc_pop_modules(mod_list, force)
+        self.dssc_pop_modules(self.modules, force)
         return 0
 
     def populate_tag(self, tag: str, force: bool = False) -> int:
@@ -119,7 +119,7 @@ class Wtf_dm(dm.Sitar_dm):
             mcache_list = [
                 mod
                 for mod in self.sitr_mods
-                if self.sitr_mods[mod]["status"] != "Update"
+                if self.sitr_mods[mod]["status"] != "Update mode"
             ]
             self.mod_list = self.get_sitr_update_list(mcache_list)
             self.sitr_populate_configs(self.sitr_mods, self.mod_list)
@@ -258,6 +258,7 @@ class Wtf_dm(dm.Sitar_dm):
         development_name = project_name.upper()
         return development_dir
 
+    # TODO - need a switch to use the existing modules populated in the WS
     def mk_branch(
         self, version: str, comment: str, tag: str = "", tapeout: str = ""
     ) -> int:
@@ -285,8 +286,10 @@ class Wtf_dm(dm.Sitar_dm):
         if self.workspace_type == "Tapeout":
             LOGGER.error("Cannot make a branch in a Tapeout workspace.")
             return 3
+        # TODO - should this be pop_modules?
         self.stclc_populate_workspace()
         self.sitr_populate_tag(self.sitr_mods, self.sitr_mods, tapeout)
+        # TODO - add a cmd line argument to skip the checks for branching
         self.mod_list = self.flat_release_submit(self.sitr_mods, tag, comment)
         self.dev_dir = development_dir
         if not self.mod_list:
@@ -373,6 +376,131 @@ class Wtf_dm(dm.Sitar_dm):
 
         return self.sitr_release(comment, email=email)
 
+    def diag(self, interactive: bool = True, do_pop: bool = False, do_scan: bool = False, do_perf: bool = False) -> bool:
+        """Run diagnostics on the modules specified"""
+        if not self.sitr_mods:
+            LOGGER.error("No SITaR modules found.")
+            return True
+        root_dir = self.stclc_get_url_root_dir(self)
+        if root_dir != os.environ['SYNC_DEVAREA_DIR']:
+            LOGGER.error(f"Invalid url root {root_dir}.")
+            return True
+        mods = self.dssc_get_all_modules()
+        if not mods:
+            LOGGER.error("No Design Sync modules found.")
+            return True
+        branch = self.stclc_get_branch() + ":"
+        mod_list = []
+        for mod in mods:
+            if not mod['modinstname'].endswith('%0'):
+                LOGGER.error(f"Bad module instance name {mod['modinstname']}.")
+                if interactive:
+                    if not self.io.prompt_to_continue(f"Remove module?"):
+                        continue
+                    self.stclc_rmfile(mod['modinstname'])
+            if mod['name'] in mod_list:
+                LOGGER.error(f"The module {mod['name']} is defined multiple times.")
+            else:
+                mod_list.append(mod['name'])
+
+        if not os.environ["SYNC_DEVAREA_TOP"] in mod_list:
+            LOGGER.error(f'The top container module {os.environ["SYNC_DEVAREA_TOP"]} was not found.')
+            return True
+
+        for mod in self.modules:
+            relpath = Path(self.sitr_mods[mod]['relpath'])
+            abspath = Path(os.environ["DSGN_PROJ"]) / relpath
+            LOGGER.debug(f"for the mod {mod}, relpath = {relpath}, abspath = {abspath}")
+            if self.sitr_mods[mod]['status'] == "NA":
+                LOGGER.error(f'The module {os.environ["SYNC_DEVAREA_TOP"]} was not found.')
+                if interactive:
+                    if not self.io.prompt_to_continue(f"Restore the {mod} module?"):
+                        continue
+                    LOGGER.info(f"Restoring {mod} to version {self.sitr_mods[mod]['baseline']}")
+                    self.stclc_update_module(mod, self.sitr_mods[mod]["baseline"])
+            elif self.sitr_mods[mod]['status'] == "Update mode":
+                if not abspath.is_dir():
+                    LOGGER.error(f"The sitr module {mod} at {self.sitr_mods[mod]['relpath']} not a directory.")
+                    if interactive:
+                        if not self.io.prompt_to_continue(f"Restore the {mod} module?"):
+                            continue
+                        LOGGER.info(f"Restoring {mod} to version {self.sitr_mods[mod]['baseline']}")
+                        self.stclc_update_module(mod, self.sitr_mods[mod]["baseline"])
+                if self.sitr_mods[mod]['selector'] != branch:
+                    LOGGER.error(f"The branch for the module {mod} is {self.sitr_mods[mod]['selector']} not {branch}.")
+                    if interactive:
+                        if not self.io.prompt_to_continue(f"Put the {mod} module back onto the {branch} branch?"):
+                            continue
+                        self.stclc_update_module(mod, self.sitr_mods[mod]["baseline"], nooverwrite=True)
+                if do_scan:
+                    # scan for modified but not locked
+                    files = self.dssc_ls_modules(mod, modified=True)
+                    mod_files = []
+                    for file in files:
+                        if file['fetchedstate'] != 'Lock':
+                            LOGGER.error(f"In the module {mod}, the file {file['name']} is modified but not locked ({file['fetchedstate']}).")
+                            mod_files.append(file)
+                    if mod_files and interactive:
+                        if self.io.prompt_to_continue(f"File a JIRA with dmrfa.help?"):
+                            # TODO - open a JIRA
+                            print("Not supported yet")
+                    # scan for unmanaged, prompt to check in?
+                    # scan for wrong group or permissions?
+                if do_pop:
+                    if interactive:
+                        if not self.io.prompt_to_continue(f"Populate the {mod} module?"):
+                            continue
+                    fname = Path(tempfile.gettempdir()) / next(tempfile._get_candidate_names())
+                    status = self.stclc_populate(mod, args=f"-log {fname}")
+                    LOGGER.info(f"Population complete. Logfile = {fname}, status = {status}")
+                    if not fname.exists():
+                        LOGGER.info(f"The output logfile {fname} was not created")
+                        continue
+                    contents = fname.read_text()
+                    overlap_def = re.compile(r'.*%0/(\S+)\s+:\s+Error: File Overlaps with Existing Unmanaged Object or Folder')
+                    overlaps = []
+                    errors = []
+                    parse = False
+                    for line in contents.splitlines():
+                        if "WARNINGS and FAILURES LISTING" in line:
+                            parse = True
+                        if not parse:
+                            continue
+                        match = overlap_def.match(line)
+                        if match:
+                            overlaps.append(match.group(1))
+                        elif 'Error:' in line :
+                            errors.append(line)
+                        elif 'Failed' in line :
+                            errors.append(line)
+                    if overlaps:
+                        for file in overlaps:
+                            path = relpath / file
+                            LOGGER.error(f"In the module {mod}, the file {relpath / file} is unmanaged, but a managed version exists in the vault.")
+                            if interactive:
+                                if not self.io.prompt_to_continue(f"Populate the version in the vault?"):
+                                    continue
+                                self.stclc_rmfile(relpath / file)
+                                self.stclc_check_out(relpath / file, locked=False)
+                    if errors:
+                        for err in errors:
+                            LOGGER.error(err)
+                        if interactive:
+                            if not self.io.prompt_to_continue(f"File a JIRA with dmrfa.help?"):
+                                continue
+                            # TODO - open a JIRA
+                            print("Not supported yet")
+            else:
+                # Check to make sure that the module is cached
+                if not abspath.is_symlink():
+                    LOGGER.error(f"The sitr module {mod} at {self.sitr_mods[mod]['relpath']} not a symlink.")
+                    if interactive:
+                        # TODO - check the mcache
+                        if not self.io.prompt_to_continue(f"Restore the {mod} module?"):
+                            continue
+                        LOGGER.info(f"Restoring {mod} to version {self.sitr_mods[mod]['baseline']}")
+                        self.stclc_update_module(mod, self.sitr_mods[mod]["baseline"])
+
     def wtf_set_dev_dir(self):
         self.force_version(self.dev_dir)
 
@@ -445,3 +573,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
